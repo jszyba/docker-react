12fc17960cfac80132bd3e1bbafc2cd4605f3e6c
Jeremy <js067410>
2019-12-27 07:22:58 -0600

    sup

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Change Me!
+          Sup!
         </p>
         <a
           className="App-link"
